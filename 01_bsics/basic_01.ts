cf625cc645097337636fa32b111f65119caf8ace
sourav-maji <s722143@gmail.com>
2025-12-30 23:25:55 +0530

    Adding command to run a ts file

--- 01_bsics/basic_01.ts
+++ 01_bsics/basic_01.ts
@@ -1,5 +1,12 @@
-function greet1(name: string): string {
+function greet_01(name: string): string {
+    // console.log("Hello");
   return "Hello, " + name;
 }
 
-console.log(greet1("sourav"));
+console.log(greet_01("sourav"));
+
+
+// run command
+// 1. sudo npm install -g typescript 
+// 2. tsc basic_01.ts 
+// 3. node basic_01.js 
