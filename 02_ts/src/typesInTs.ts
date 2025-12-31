5ba9d12733d5d53e613b5cb818d834428c31940e
sourav-maji <s722143@gmail.com>
2025-12-31 10:35:40 +0530

    adding error message

--- 02_ts/src/typesInTs.ts
+++ 02_ts/src/typesInTs.ts
@@ -1,6 +1,16 @@
 let drink = "chai";
 
-// drink = 0  // Type 'number' is not assignable to type 'string'.ts(2322)
+// drink = 0  // error : Type 'number' is not assignable to type 'string'.ts(2322)
 
 let cups = Math.random() > 0.5 ? 10 : 5;
 console.log(cups);
+
+let channelName = "Chaiaurcode"
+// channelName = 123333  // error : Type 'number' is not assignable to type 'string'.
+
+let chaiFlavour : string = "Malasa chai"
+chaiFlavour = "Ginger tea"
+// chaiFlavour = 2 // error: Type 'number' is not assignable to type 'string'.ts(2322)
+
+
+// datatypes : boolean,string, number
