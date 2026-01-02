4deaf0326e2caf1783b5b7b333837c19eec27ef2
sourav-maji <s722143@gmail.com>
2026-01-02 22:20:59 +0530

    Constructor in ts

---
+++ 02_ts/src/oop.ts
@@ -0,0 +1,15 @@
+// class Chai {
+//   flavour: string;
+//   price: number;
+
+//   //   constructor(flavour: string, price: number) {
+//   //     (this.flavour = flavour), (this.price = price);
+//   //   }
+//   constructor(flavour: string) {
+//     this.flavour = flavour;
+//     console.log(this);
+//   }
+// }
+
+// const masalaChai = new Chai("Ginger", 20);
+
