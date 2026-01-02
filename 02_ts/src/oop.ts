6331991341db7493852d9fdd8bdd6fd9134832b5
sourav-maji <s722143@gmail.com>
2026-01-02 22:38:22 +0530

    readonly,abstract class in ts

--- 02_ts/src/oop.ts
+++ 02_ts/src/oop.ts
@@ -37,3 +37,73 @@ class Branch extends Shop {
 }
 
 new Branch().getName();
+
+class Walet {
+  // private modifier
+
+  #balance = 100;
+  getBalance() {
+    return this.#balance;
+  }
+}
+
+const w = new Walet();
+
+class Cup {
+  readonly capacity: number = 250;
+
+  constructor(capacity: number) {
+    this.capacity = capacity;
+  }
+}
+
+class ModernChai {
+  private _sugar: number = 2;
+
+  get sugar() {
+    return this.sugar;
+  }
+  set sugar(value: number) {
+    if (value > 5) {
+      throw new Error("Too Sweet");
+    }
+    this._sugar = value;
+  }
+}
+
+const d = new ModernChai();
+d.sugar = 3;
+
+class EkChai {
+  static shopName = "Chaicode cafe";
+
+  constructor(public flavour: string) {}
+}
+
+console.log(EkChai.shopName);
+
+// abstract class
+
+abstract class Drink {
+  abstract make(): void;
+}
+
+class MyChai extends Drink {
+  make(): void {
+    console.log("Brewing chai");
+  }
+}
+
+class Heater{
+  heat(){
+
+  }
+}
+
+class ChaiMaker{
+  constructor(private heater: Heater){}
+
+  make(){
+    this.heater.heat
+  }
+}
